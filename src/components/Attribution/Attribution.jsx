7d7276ff25a3b256e69644c2dfe764000fe64929
mmtakeuchi <mikemtaki@gmail.com>
2021-11-02 12:48:06 -0700

    add sass for styling and created basic variables from the style guide

---
+++ src/components/Attribution/Attribution.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Attibution.scss";
+
+const Attribution = () => {
+  return (
+    <div class="attribution">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://github.com/mmtakeuchi/easybank-landing"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Michael Takeuchi
+      </a>
+      .
+    </div>
+  );
+};
+
+export default Attribution;
